7d752170aa32a7cebcce3af6685226c4197cd606
Zeph Lafassett <zeph@lafassett.com>
2013-10-26 11:27:34 -0700

    adding index.js and turned server.js into node module

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 var http = require("http");
 
-var onRequest = function(request, response) {
-	response.writeHead(200, {"Content-Type:": "text/plain"});
-	response.write("Hello World")
-	response.end();
-};
+function start() {
+	var onRequest = function(request, response) {
+		console.log("Request received.");
+		response.writeHead(200, {"Content-Type:": "text/plain"});
+		response.write("Hello World")
+		response.end();
+	};
 
-http.createServer(onRequest).listen(8888);
+	http.createServer(onRequest).listen(8888);
+	console.log("Server started");
+}
+
+exports.start = start;
