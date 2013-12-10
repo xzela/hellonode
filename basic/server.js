f977343ec908bb75203171bb4d1b7ad1837f37a2
Zeph Lafassett <zeph@lafassett.com>
2013-12-10 14:05:42 -0800

    adding route to index.js not server.js

--- basic/server.js
+++ basic/server.js
@@ -1,11 +1,12 @@
 var http = require('http');
 var url = require('url');
-var router = require('./router');
 
-function start() {
+function start(route) {
 	function onRequest(request, response) {
 		var pathname = url.parse(request.url).pathname;
-		router.route(pathname);
+
+		route(pathname);
+
 		response.writeHead(200, {'Content-Type': 'text/plain'});
 		response.write('hello world');
 		response.end();
