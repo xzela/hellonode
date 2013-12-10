a1d25fdd6f102fdf197cf43eb5de6f5abbb3fee2
Zeph Lafassett <zeph@lafassett.com>
2013-12-10 15:13:53 -0800

    adding formidable module to file upload support

--- basic/requestHandler.js
+++ basic/requestHandler.js
@@ -1,4 +1,6 @@
-var querystring = require('querystring');
+var querystring = require('querystring'),
+	fs = require('fs'),
+	formidable = require('formidable');
 
 function start(response) {
 	console.log("Request handler 'start' was called.");
@@ -8,9 +10,9 @@ function start(response) {
 		'charset=UTF-8" />'+
 		'</head>'+
 		'<body>'+
-		'<form action="/upload" method="post">'+
-		'<textarea name="text" rows="20" cols="60"></textarea>'+
-		'<input type="submit" value="Submit text" />'+
+		'<form action="/upload" method="post" enctype="multipart/form-data">'+
+		'<input type="file" name="upload" />'+
+		'<input type="submit" value="Submit file" />'+
 		'</form>'+
 		'</body>'+
 		'</html>';
@@ -19,15 +21,35 @@ function start(response) {
 		response.end();
 }
 
-function upload(response, postData) {
+function upload(response, request) {
 	console.log("Request handler 'upload' was called.");
 
-	response.writeHead(200, {'Content-Type': 'text/plain'});
-	response.write("You've sent: " + querystring.parse(postData).text);
+	var form = new formidable.IncomingForm();
+	console.log('about to parse');
+	form.parse(request, function(error, fields, files){
+		console.log('parsing done');
+		fs.rename(files.upload.path, "/tmp/test.png", function(error) {
+			if (error) {
+				fs.unlink("/tmp/test.png");
+				fs.rename(files.upload.path, "/tmp/test.png");
+			}
+		});
+	});
+
+	response.writeHead(200, {'Content-Type': 'text/html'});
+	response.write("recived image: <br />");
+	response.write("<img src='/show' />");
 	response.end();
 
 }
 
+function show(response) {
+	console.log("Request handler 'show' was called");
+
+	response.writeHead(200, {"Content-Type": 'image/png'});
+	fs.createReadStream("/tmp/test.png").pipe(response);
+}
+
 exports.start = start;
 exports.upload = upload;
-
+exports.show = show;
--- basic/server.js
+++ basic/server.js
@@ -3,18 +3,9 @@ var url = require('url');
 
 function start(route, handle) {
 	function onRequest(request, response) {
-		var postData = "";
 		var pathname = url.parse(request.url).pathname;
 		console.log("Request for " + pathname + " received");
-		request.setEncoding('utf8');
-
-		request.addListener("data", function(postDataChunk) {
-			postData += postDataChunk;
-			console.log("Recieved POST data chunk '" + postDataChunk + "'.");
-		});
-		request.addListener('end', function() {
-			route(handle, pathname, response, postData);
-		});
+		route(handle, pathname, response, request);
 	}
 
 	http.createServer(onRequest).listen(3000);
