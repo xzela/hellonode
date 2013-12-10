359a10caa7009c03b136096b9e7352608756714e
Zeph Lafassett <zeph@lafassett.com>
2013-12-10 13:08:34 -0800

    moving server logic into its own module

--- basic/server.js
+++ basic/server.js
@@ -1,11 +1,15 @@
 var http = require('http');
-function onRequest(request, response) {
-	console.log("Request received");
-	response.writeHead(200, {'Content-Type': 'text/plain'});
-	response.write('hello world');
-	response.end();
-}
-var server = http.createServer(onRequest);
-server.listen(3000);
 
-console.log("Server has Started.");
+function start() {
+	function onRequest(request, response) {
+		console.log("Request received");
+		response.writeHead(200, {'Content-Type': 'text/plain'});
+		response.write('hello world');
+		response.end();
+	}
+
+	http.createServer(onRequest).listen(3000);
+	console.log("Server has Started.");
+}
+
+exports.start = start;
