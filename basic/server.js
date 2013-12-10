7079132faea15bcd641b83fe7c5d470b766b6cb3
Zeph Lafassett <zeph@lafassett.com>
2013-12-10 14:53:31 -0800

    accepting post data

--- basic/server.js
+++ basic/server.js
@@ -3,10 +3,18 @@ var url = require('url');
 
 function start(route, handle) {
 	function onRequest(request, response) {
+		var postData = "";
 		var pathname = url.parse(request.url).pathname;
 		console.log("Request for " + pathname + " received");
+		request.setEncoding('utf8');
 
-		route(handle, pathname, response);
+		request.addListener("data", function(postDataChunk) {
+			postData += postDataChunk;
+			console.log("Recieved POST data chunk '" + postDataChunk + "'.");
+		});
+		request.addListener('end', function() {
+			route(handle, pathname, response, postData);
+		});
 	}
 
 	http.createServer(onRequest).listen(3000);
